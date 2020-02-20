de1e8c8f0bd0df241b4d806c88212f76bd3b89c7
DESKTOP-9EKINND\Abhi <abhi.sculptsoft@gmail.com>
2020-02-20 16:54:03 +0530

    Logger added to CarDetailsService

--- src/main/java/com/interview/example/service/CarDetailsService.java
+++ src/main/java/com/interview/example/service/CarDetailsService.java
@@ -3,6 +3,8 @@ package com.interview.example.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,32 +17,34 @@ import com.interview.example.repository.CarManufacturerRepository;
 
 @Service
 public class CarDetailsService {
-	
-	@Autowired
-	CarManufacturerRepository carManufacturerRepository;
-	
-	
-	@Autowired
-	CarDetailsRepository carDetailsRepository;
-	
-	public List<CarManufacturerModel> getCarManufacturers(){
-		
-		return this.carManufacturerRepository.findAll();
-	}
-	
-	public List<CarDetailsModel> getCarsDetailsByBrand(SearchByBrandIdModel byBrandIdModel){
-		
-		String brandId =  byBrandIdModel.getBrandId();
-		
-		List<CarDetailsModel> carDetailsModels = new ArrayList<CarDetailsModel>();
-		carDetailsModels = carDetailsRepository.findByBrandId(brandId);
-		return carDetailsModels;
-	}
-	
-	public ResponseEntity<CarDetailsModel> addCarDetails(CarDetailsModel carDetailsModel){
-		carDetailsRepository.save(carDetailsModel);
-		return ResponseEntity.ok(carDetailsModel);
-	}
-	
+
+  private static final Logger logger = LogManager.getLogger(CarDetailsService.class);
+
+  @Autowired
+  CarManufacturerRepository carManufacturerRepository;
+
+
+  @Autowired
+  CarDetailsRepository carDetailsRepository;
+
+  public List<CarManufacturerModel> getCarManufacturers() {
+
+    return this.carManufacturerRepository.findAll();
+  }
+
+  public List<CarDetailsModel> getCarsDetailsByBrand(SearchByBrandIdModel byBrandIdModel) {
+
+    String brandId = byBrandIdModel.getBrandId();
+
+    List<CarDetailsModel> carDetailsModels = new ArrayList<CarDetailsModel>();
+    carDetailsModels = carDetailsRepository.findByBrandId(brandId);
+    return carDetailsModels;
+  }
+
+  public ResponseEntity<CarDetailsModel> addCarDetails(CarDetailsModel carDetailsModel) {
+    carDetailsRepository.save(carDetailsModel);
+    return ResponseEntity.ok(carDetailsModel);
+  }
+
 
 }
